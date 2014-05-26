e207fd6a745184aa6f53659f47216bd84866b771
student <student@PC111S32.fgakusei.local>
2014-05-26 13:54:50 +0900

    2回

--- gen/com/example/hellogalaxy/R.java
+++ gen/com/example/hellogalaxy/R.java
@@ -25,17 +25,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
-        public static final int button1=0x7f080009;
-        public static final int button2=0x7f080008;
-        public static final int container=0x7f080000;
-        public static final int editText1=0x7f080006;
-        public static final int editText2=0x7f080003;
-        public static final int linearLayout1=0x7f080001;
-        public static final int multiAutoCompleteTextView1=0x7f080004;
-        public static final int multiAutoCompleteTextView2=0x7f080007;
-        public static final int tableRow1=0x7f080002;
-        public static final int tableRow2=0x7f080005;
+        public static final int action_settings=0x7f08000d;
+        public static final int button1=0x7f080006;
+        public static final int button2=0x7f080007;
+        public static final int editText1=0x7f08000b;
+        public static final int editText2=0x7f080009;
+        public static final int familyNameText=0x7f080005;
+        public static final int firstNameText=0x7f080002;
+        public static final int linearLayout1=0x7f080008;
+        public static final int multiAutoCompleteTextView1=0x7f08000a;
+        public static final int multiAutoCompleteTextView2=0x7f08000c;
+        public static final int tableRow1=0x7f080000;
+        public static final int tableRow2=0x7f080003;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,7 +50,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int button_cancel=0x7f050007;
+        public static final int button_ok=0x7f050006;
+        public static final int family_name=0x7f050004;
+        public static final int first_name=0x7f050003;
         public static final int hello_world=0x7f050001;
+        public static final int please_name=0x7f050005;
     }
     public static final class style {
         /** 
