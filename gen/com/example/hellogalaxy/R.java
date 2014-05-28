7a3fe9a106dcf8bdae3d4909312ca18662f240fb
student <student@PC111S32.fgakusei.local>
2014-05-28 10:18:10 +0900

    2

--- gen/com/example/hellogalaxy/R.java
+++ gen/com/example/hellogalaxy/R.java
@@ -25,20 +25,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000d;
-        public static final int button1=0x7f080006;
+        public static final int action_settings=0x7f08000e;
+        public static final int btnOK=0x7f080006;
         public static final int button2=0x7f080007;
-        public static final int editText1=0x7f08000b;
-        public static final int editText2=0x7f080009;
-        public static final int familyNameText=0x7f080005;
-        public static final int firstNameText=0x7f080002;
-        public static final int linearLayout1=0x7f080008;
-        public static final int multiAutoCompleteTextView1=0x7f08000a;
-        public static final int multiAutoCompleteTextView2=0x7f08000c;
+        public static final int editText1=0x7f08000c;
+        public static final int editText2=0x7f08000a;
+        public static final int edtFamily=0x7f080005;
+        public static final int edtFirst=0x7f080002;
+        public static final int linearLayout1=0x7f080009;
+        public static final int multiAutoCompleteTextView1=0x7f08000b;
+        public static final int multiAutoCompleteTextView2=0x7f08000d;
         public static final int tableRow1=0x7f080000;
         public static final int tableRow2=0x7f080003;
         public static final int textView1=0x7f080001;
         public static final int textView2=0x7f080004;
+        public static final int txvMsg=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
